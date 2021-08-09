4bed60f66af07e8770fd67f70fa96359bb23427e
Wildan F Purnomo <wildanfajarpurnomo@gmail.com>
2021-08-09 12:56:11 +0700

    Done hot reload storybook but still slow on webpack build

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,5 +6,17 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
-  ]
+  ],
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Make whatever fine-grained changes you need;
+    config.watchOptions.aggregateTimeout = 300
+    config.watchOptions.poll = 1000  
+
+    // Return the altered config
+    return config;
+  },
 }
